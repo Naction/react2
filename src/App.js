1d670f16de411b2456ff65e4c0c8477d37bca488
古藤勇魚 <>
2020-03-12 11:21:45 +0900

    ルーティング

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-function App() {
-  return(
-    <div className="container text-center">
-      <Clock />
+const App = () => (
+  <BrowserRouter>
+    <div class="container text-center mt-5">
+      <Route exact path="/" component={Home} />
     </div>
-    
-    )
-}
+  </BrowserRouter>
+)
 
-class Clock extends React.Component{
-  constructor(props){
-    super(props);
-    this.now = new Date();
-
-
-    this.state = {
-     time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
-    }
-
-    this.refresh = this.refresh.bind(this);
+const Home = () =>{
+  return(
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  )
   }
 
-  refresh(){
-    this.now = new Date();
-    this.setState((state) =>({
-     time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
-    }));
-  }
-  render(){
-    return <p onClick={this.refresh}>{this.state.time}</p>
-  }
+export default App
 
-}
-
-export default App;
