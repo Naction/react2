c9d1c55162c53bc535ed002a1b12d8389a13f281
古藤勇魚 <>
2020-03-12 11:42:31 +0900

    ルーティング完了

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ const App = () => (
   <BrowserRouter>
     <div class="container text-center mt-5">
       <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
+      <Route path="/about" component={About} />
+      <Route path="/blog/:id" component={Blog} />
+      <Route path="/sum/:num1/:num2" component={Sum} />
     </div>
   </BrowserRouter>
 )
@@ -14,6 +16,8 @@ const Home = () =>{
   return(
     <div>
       <h1>Welcome</h1>
+
+      <p><Link to="/about">About</Link></p>
     </div>
   )
   }
@@ -21,6 +25,24 @@ const About = () =>{
   return(
     <div>
       <h1>About</h1>
+    </div>
+  )
+  }
+const Blog = props =>{
+  const { id } = props.match.params;
+
+  return(
+    <div>
+      <p>{id}番目の記事です</p>
+    </div>
+  )
+  }
+const Sum = props =>{
+  const { num1, num2 } = props.match.params;
+
+  return(
+    <div>
+      <p>{num1}+{num2} = {parseInt(num1) + parseInt(num2)}</p>
     </div>
   )
   }
